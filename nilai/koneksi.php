d3ea5c412e6417016fa32151e6501e604a158a62
Muhamad Taopik <52822242+Mr94t3z@users.noreply.github.com>
2021-11-29 19:05:47 +0700

    input nilai mahasiswa

---
+++ nilai/koneksi.php
@@ -0,0 +1,11 @@
+<?php
+$hostname = "localhost";
+$username = "root";
+$password = "";
+$database = "prak_paw";
+
+$conn = mysqli_connect($hostname, $username, $password, $database);
+// script cek koneksi
+if (!$conn) {
+    die("Koneksi Tidak Berhasil: " . mysqli_connect_error());
+}
